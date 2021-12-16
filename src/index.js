4ff3f93e12f66663cfb471b61aaa19e478fcd8e0
Buyaki <rittahbuyaki@gmail.com>
2021-12-16 23:02:59 +0300

    Use of arrow functions instead of the function keyword

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import './style.css';
 
 const tableBody = document.querySelector('tbody');
 const requestURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/XlbFBk8CtDvgI6FEd2iz/scores/';
-function scoreElement(name, score) {
+
+const scoreElement = (name, score) => {
   const tableRow = document.createElement('tr');
   const tableData = document.createElement('td');
 
@@ -10,27 +11,22 @@ function scoreElement(name, score) {
 
   tableRow.appendChild(tableData);
   tableBody.appendChild(tableRow);
-}
-async function getScores() {
-  // Get scores data
-
+};
+const getScores = async () => {
   const data = await fetch(requestURL);
-
-  // convert scores data to object/array
   const response = await data.json();
-
-  // find scores array
   const scores = await response.result;
   return scores;
-}
-async function addScores(user, score) {
+};
+
+const addScores = async (user, score) => {
   fetch(requestURL, {
     method: 'POST',
     body: JSON.stringify({ user, score }),
     mode: 'cors',
     headers: { 'Content-type': 'application/json' },
   });
-}
+};
 const inputForm = document.querySelector('.addItem');
 inputForm.addEventListener('submit', (e) => {
   e.preventDefault();
@@ -39,10 +35,12 @@ inputForm.addEventListener('submit', (e) => {
   addScores(getName, getScore);
   inputForm.reset();
 });
-async function displayAllScores() {
+
+const displayAllScores = async () => {
   const allScores = await getScores();
   allScores.forEach((score) => scoreElement(score.user, score.score));
-}
+};
+
 const refreshBtn = document.querySelector('.refresh');
 refreshBtn.addEventListener('click', displayAllScores);
 displayAllScores();
